1ce85cfb3795e6ad57f521b700b4a415b637f2e0
Debondt Didier <p@henix.be>
2015-09-23 15:22:30 +0200

    Init

---
+++ lang/paquet-dev_vertige_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'dev_vertige_description' => '',
+	'dev_vertige_nom' => 'Vertige Dev',
+	'dev_vertige_slogan' => 'Installer les plugins de développment'
+);
